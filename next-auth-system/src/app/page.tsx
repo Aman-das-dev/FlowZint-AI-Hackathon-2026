aeeb2e789621e9cbac48a158e3a3d78aa6182852
Aman-das-dev <sahebprasaddas12@gmail.com>
2026-07-09 20:30:32 +0530

    Install client-side supabase-js & ssr packages and set up next-auth-system supabase config helpers

--- next-auth-system/src/app/page.tsx
+++ next-auth-system/src/app/page.tsx
@@ -1,12 +1,17 @@
-import { auth } from "@/auth";
-import { redirect } from "next/navigation";
+import { createClient } from '@/utils/supabase/server'
+import { cookies } from 'next/headers'
 
-export default async function Home() {
-  const session = await auth();
-  
-  if (session) {
-    redirect("/dashboard");
-  } else {
-    redirect("/login");
-  }
+export default async function Page() {
+  const cookieStore = await cookies()
+  const supabase = createClient(cookieStore)
+
+  const { data: todos } = await supabase.from('todos').select()
+
+  return (
+    <ul>
+      {todos?.map((todo) => (
+        <li key={todo.id}>{todo.name}</li>
+      ))}
+    </ul>
+  )
 }
