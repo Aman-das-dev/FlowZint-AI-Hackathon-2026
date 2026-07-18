a30e0e30c8d8763d2027ad0134b6ba14c7b8ed43
Aman-das-dev <sahebprasaddas12@gmail.com>
2026-07-18 23:43:29 +0530

    Revert "Remove Pipfile to enforce pip requirements.txt and add basic HTTP handler to test"
    This reverts commit 446761e1811ec4b892d7fad3d8b964c22acc9aeb.

--- api/test.py
+++ api/test.py
@@ -1,11 +1,7 @@
-from http.server import BaseHTTPRequestHandler
-import sys
-import os
+from fastapi import FastAPI
 
-class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        self.wfile.write(f'Test successful. Python {sys.version}'.encode('utf-8'))
-        return
+app = FastAPI()
+
+@app.get("/api/test")
+def test():
+    return {"message": "Test successful"}
